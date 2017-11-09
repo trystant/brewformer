02ae449763874117382c89a542b260b83b4cef2b
Andrew Kadesky <amkadesky@aol.com>
2017-11-09 09:47:04 -0600

    website now recieves all breweries from dallas,TX and is logged in the console. Search button added, recieving error that search function is not found.

--- config.js
+++ config.js
@@ -6,7 +6,9 @@ requirejs.config({
 		jquery: 'jquery-3.2.1.min',
 		materialize: ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize', 'materialize.min'],
 		velocity: 'velocity.min',
-		methods: 'Custom-scripts/methods'
+		methods: 'Custom-scripts/methods',
+		auto: 'https://cdnjs.cloudflare.com/ajax/libs/materialize-autocomplete/1.0.7/jquery.materialize-autocomplete.min',
+		hammer: 'hammer.min'
 	},
 
 	shim: {
--- js/Custom-scripts/methods.js
+++ js/Custom-scripts/methods.js
@@ -1,111 +1,31 @@
-require(['jquery', 'materialize'], function($, Velocity){
+require(['jquery', 'materialize', 'auto', 'hammer'], function($, Velocity){
 
-		  	var currentCities=[];
-	// This is a demo API key that can only be used for a short period of time, and will be unavailable soon. You should rather request your API key (free)  from http://battuta.medunes.net/ 	
-	var BATTUTA_KEY= "bb03b960b7d675d47e5d78b1795da95c";
 	var BREW_KEY= "bb5091fd74b7944bb27ac0fd535524ef";
-	  	// Populate country select box from battuta API
-		url="https://battuta.medunes.net/api/country/all/?key="+BATTUTA_KEY +"&callback=?";
-	  	$.getJSON(url,function(countries)
-	  	{
-	  		console.log(countries);
-	      $('#country').material_select();
-		    //loop through countries..
-		    // $.each(countries,function(key,country)
-		    // {
-		        $("<option></option>")
-		         				.attr("value", "us")
-		         				.append("United States of America")
-		                     	.appendTo($("#country"));
-		       
-		    // }); 
-		    // trigger "change" to fire the #state section update process
-		    $("#country").material_select('update');
-		    $("#country").trigger("change");
-		    
 
-	  	});
-	    
-	    $("#country").on("change",function()
-	  	{
-	  	
-	  		countryCode= $("#country").val();
-	  		
-	  		// Populate country select box from battuta API
-		    url="https://battuta.medunes.net/api/region/"
-		    +countryCode
-		    +"/all/?key="+BATTUTA_KEY+"&callback=?";
-
-	  		$.getJSON(url,function(regions)
-	  		{
-	  			$("#region option").remove();
-			    //loop through regions..
-			    $.each(regions,function(key,region)
-			    {
-			        $("<option></option>")
-			         				.attr("value",region.region)
-			         				.append(region.region)
-			                     	.appendTo($("#region"));
-			    });
-			    // trigger "change" to fire the #state section update process
-		    	$("#region").material_select('update');
-		    	$("#region").trigger("change");
-		    	
-		    }); 
-		    
-	  	});
-	  	$("#region").on("change",function()
-	  	{
-	  		
-	  		// Populate country select box from battuta API
-	  		countryCode=$("#country").val();
-			region=$("#region").val();
-		    url="https://battuta.medunes.net/api/city/"
-		    +countryCode
-		    +"/search/?region="
-		    +region
-		    +"&key="
-		    +BATTUTA_KEY
-		    +"&callback=?";
-	  		
-	  		$.getJSON(url,function(cities)
-	  		{
-	  			console.log(cities);
-	  			currentCities=cities;
-	        	var i=0;
-	        	$("#city option").remove();
-	        
-			    //loop through regions..
-			    $.each(cities,function(key,city)
-			    {
-			        $("<option></option>")
-			         				.attr("value",i++)
-			         				.append(city.city)
-			                .appendTo($("#city"));
-			    });
-			    // trigger "change" to fire the #state section update process
-		    	$("#city").material_select('update');
-		    	$("#city").trigger("change");
-		    	
-		    }); 
-		    
-	  	});	
-	  	$("#city").on("change",function()
-	  	{
-	      url = "http://api.brewerydb.com/v2/?&key="+BREW_KEY+"&callback=?";
-	      $.getJSON(url, function(breweries){
-	      	console.log(breweries);
-	      })
-	      currentIndex=$("#city").val();
-	      currentCity=currentCities[currentIndex];
-	      city=currentCity.city;
-	      region=currentCity.region;
-	      country=currentCity.country;
-	      lat=currentCity.latitude;
-	      lng=currentCity.longitude;
-	      $("#location").html('<i class="fa fa-map-marker"></i> <strong> '+city+"/"+region+"</strong>("+lat+","+lng+")");
-	      $("#selection").html('<span class="random-sentence">You have selected ' + city + ' ' + region + '</span>');
-	    });
-	   //-------------------------------END OF SELECT CASCADING-------------------------//
-
+	var autocomplete = $('#el').materialize_autocomplete({
+    limit: 20,
+    multiple: {
+        enable: true,
+        maxSize: 10,
+        onExist: function (item) { /* ... */ },
+        onExceed: function (maxSize, item) { /* ... */ }
+    },
+    appender: {
+        el: '#someEl'
+    },
+    getData: function (value, callback) {
+        // ...
+        callback(value, data);
+    }
+});
+	//function search(){
+		const proxyurl = "https://cors-anywhere.herokuapp.com/"
+	    url = "http://api.brewerydb.com/v2/locations?region=texas&ocality=dallas&key="+BREW_KEY+"&callback="+function(){
+	    	return true;
+	    }+"?";
+	    fetch(proxyurl + url).then(respones=> respones.text()).then(contents=>console.log(contents)).catch(console.log("can't access "+ url + "response. Blocked by browser?"));
+	    // $.getJSON(url, function(breweries){
+	    //   console.log(breweries);
+	    // });
+	// }
 });
