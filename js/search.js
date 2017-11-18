1bca059bc0188d0ff020d2e0933e7c0ee38343e1
Andrew Kadesky <amkadesky@aol.com>
2017-11-18 10:52:49 -0600

    search function now console.logs all brewery names in result array. eslint was installed.

--- js/search.js
+++ js/search.js
@@ -1,14 +1,23 @@
-	const BREW_KEY= "bb5091fd74b7944bb27ac0fd535524ef";
-	function search(){
-		var region = $("#Region").val();
-		var city = $("City").val();
-		const proxyurl = "https://cors-anywhere.herokuapp.com/"
-	    url = "http://api.brewerydb.com/v2/locations?region=texas&locality=dallas&isPrimary=Y&isClosed=N&key="+BREW_KEY+"&callback="+function(){
-	    	return true;
-	    }+"?";
-	    fetch(proxyurl + url).then(respones=> respones.text()).then(contents=>console.log(contents)).catch(console.log("can't access "+ url + "response. Blocked by browser?"));
+const BREW_KEY= "bb5091fd74b7944bb27ac0fd535524ef";
+function search(){
+    var region = $("#Region").val();
+    var city = $("City").val();
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"
+    url = "http://api.brewerydb.com/v2/locations?region=texas&locality=dallas&isPrimary=Y&isClosed=N&key="+BREW_KEY+"&callback="+function(){
+        return true;
+    }+"?";
+    fetch(proxyurl + url).then(respones=> respones.text()).then(contents=>getBreweries(contents)).catch(console.log("can't access "+ url + "response. Blocked by browser?"));
 
-	}
+}
 
-	//Use JSON.parse() to comb through data object created by
-	//berwerydb to get names of breweries.
+//Use JSON.parse() to comb through data object created by
+//berwerydb to get names of breweries.
+
+function getBreweries(contents){
+    var list = JSON.parse(contents);
+    for(var i = 0; i <= list.data.length; i++){
+        var brewery = list.data[i].name;
+        console.log(brewery);
+    }
+    console.log(contents);
+}
