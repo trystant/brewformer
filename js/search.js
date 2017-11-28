edab4ca4e781ccb9e6fd72d9660e4bd71f5cbe5f
Andrew Kadesky <amkadesky@aol.com>
2017-11-28 14:02:49 -0600

    request will now use values inputted into text fields.

--- js/search.js
+++ js/search.js
@@ -1,16 +1,10 @@
-require(['jquery', 'materialize', 'search', 'auto', 'hammer'], function($, Velocity,search,auto){
 const BREW_KEY= "bb5091fd74b7944bb27ac0fd535524ef";
-$('#City').on( "change", function(){
-        var city = $("#City").val();
-})
-$('#Region').on( "change", function(){
-        var region = $("#Region").val();
-})
 function search(){
     $('#brewery-list').empty();
-
+    var region = $("#Region").val();
+    var city = $("#City").val();
     const proxyurl = "https://cors-anywhere.herokuapp.com/"
-    url = "http://api.brewerydb.com/v2/locations?region=texas&locality=dallas&isPrimary=Y&isClosed=N&key="+BREW_KEY+"&callback="+function(){
+    url = "http://api.brewerydb.com/v2/locations?region="+region+"&locality="+city+"&isPrimary=Y&isClosed=N&key="+BREW_KEY+"&callback="+function(){
         return true;
     }+"?";
     fetch(proxyurl + url).then(respones=> respones.text()).then(contents=>getBreweries(contents)).catch(console.log("can't access "+ url + "response. Blocked by browser?"));
@@ -24,9 +18,8 @@ function getBreweries(contents){
     var list = JSON.parse(contents);
     for(var i = 0; i < list.data.length; i++){
         var brewery = list.data[i].brewery.name;
-        var item = "<div class='col-3'><span id='list-item'>"+brewery+"</span></div>"
+        var item = "<div id='item-container' class='col-3'><span id='list-item'>"+brewery+"</span></div>"
         $('#brewery-list').append(item);
         console.log(brewery);
     }
 }
-})
