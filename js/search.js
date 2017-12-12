f3193c6c233f65571162fafecdc9a59fdb2cbba7
Andrew Kadesky <amkadesky@aol.com>
2017-12-12 17:14:42 -0600

    search.js will now send an api request to brewerydb for each beer that is found in the location the user searches for.

--- js/search.js
+++ js/search.js
@@ -1,13 +1,14 @@
 const BREW_KEY= "bb5091fd74b7944bb27ac0fd535524ef";
 function search(){
     $('#brewery-list').empty();
+    $('#error-container').empty();
     var region = $("#Region").val();
     var city = $("#City").val();
     const proxyurl = "https://cors-anywhere.herokuapp.com/"
     url = "http://api.brewerydb.com/v2/locations?region="+region+"&locality="+city+"&isPrimary=Y&isClosed=N&key="+BREW_KEY+"&callback="+function(){
         return true;
     }+"?";
-    fetch(proxyurl + url).then(respones=> respones.text()).then(contents=>getBreweries(contents)).catch(console.log("can't access "+ url + "response. Blocked by browser?"));
+    fetch(proxyurl + url).then(response=> response.text()).then(contents=>getBreweries(contents)).catch(error => errorMessage(error));
 
 }
 
@@ -19,7 +20,22 @@ function getBreweries(contents){
     for(var i = 0; i < list.data.length; i++){
         var brewery = list.data[i].brewery.name;
         var item = "<div class='col-3 item-container'><span class='list-item' aria-live='assertive'>"+brewery+"</span></div>"
-        $('#brewery-list').append(item);
-        console.log(brewery);
+        $('#brewery-list').append(item).fadeIn(999);
+        // console.log(brewery);
+        getBeers(list, i);
     }
+}
+
+function getBeers(list, i){
+    var brewery_id = list.data[i].brewery.id;
+    console.log(brewery_id);
+    var url = "http://api.brewerydb.com/v2/brewery/"+brewery_id+"/beers?key="+BREW_KEY;
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    fetch(proxyurl + url).then(response=> response.text());
+}
+
+function errorMessage(error){
+    console.log(error);
+    var message = "<span class='error-message'>There seems to be something wrong with your search entry.  Check to see if you have left either field blank or mispelled either your entered state and/or city.</span>"
+    $('#error-container').append(message).fadeIn(999);
 }
