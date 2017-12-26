2b2746917d7253b124da1c60c46ab90d4f2ec1fd
Andrew Kadesky <amkadesky@aol.com>
2017-12-26 14:45:50 -0600

    tooltip init function moved to methods.js

--- js/Custom-scripts/methods.js
+++ js/Custom-scripts/methods.js
@@ -1,6 +1,9 @@
 require(['jquery', 'materialize', 'search', 'auto', 'hammer'], function($, Velocity,search,auto){
     var toolTipText = "Click Me!"
 	$('.section-input').append("<button id='submit' data-toggle='tooltip' title='"+toolTipText+"' onclick='search()''>Search</button>");
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 	var autocomplete = $('#Region').materialize_autocomplete({
     limit: 20,
     multiple: {
