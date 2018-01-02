a340ce00acc0d99b748c951ce8d061e2de8f667e
Andrew Kadesky <amkadesky@aol.com>
2018-01-02 12:22:10 -0700

    search button now used materializes tooltip style.

--- js/Custom-scripts/methods.js
+++ js/Custom-scripts/methods.js
@@ -1,24 +1,25 @@
-require(['jquery', 'materialize', 'search', 'auto', 'hammer'], function($, Velocity,search,auto){
-    var toolTipText = "Click Me!"
-	$('.section-input').append("<button id='submit' data-toggle='tooltip' title='"+toolTipText+"' onclick='search()''>Search</button>");
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-	var autocomplete = $('#Region').materialize_autocomplete({
-    limit: 20,
-    multiple: {
-        enable: true,
-        maxSize: 10,
-        onExist: function (item) { /* ... */ },
-        onExceed: function (maxSize, item) { /* ... */ }
-    },
-    appender: {
-        el: '#Region'
-    },
-    getData: function (value, callback) {
-        // ...
-        callback(value, data);
-    }
-});
+require(['jquery', 'materialize', 'search', 'auto', 'hammer'], function($,Velocity,search,auto){
+
+    $('.section-input').append("<button id='submit' class='btn tooltipped' data-position='bottom' data-delay='50' data-tooltip='Click Me!' onclick='search()''>Search</button>");
+    // $('.section-input').append("<span class='tool-tip' data-toggle='tooltip' data-placement='top' title='Tooltip on top'><button disabled='disabled'>I am disabled</button></span>");
+    // $(document).ready(function(){
+    //     $('[data-toggle="tooltip"]').tlp();
+    // });
+// 	var autocomplete = $('#Region').materialize_autocomplete({
+//     limit: 20,
+//     multiple: {
+//         enable: true,
+//         maxSize: 10,
+//         onExist: function (item) { /* ... */ },
+//         onExceed: function (maxSize, item) { /* ... */ }
+//     },
+//     appender: {
+//         el: '#Region'
+//     },
+//     getData: function (value, callback) {
+//         // ...
+//         callback(value, data);
+//     }
+// });
 
 });
